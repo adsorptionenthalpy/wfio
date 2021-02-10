9e2a97280a660728bc780bccdbb29daddbb9fe51
BlockTomeLTD <BlockTomeLTD@gmail.com>
2021-02-10 16:04:50 -0700

    add erc20burnable

--- contracts/wfio.sol
+++ contracts/wfio.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract WFIO is ERC20 {
+contract WFIO is ERC20, ERC20Burnable {
 
     constructor(uint256 _initialSupply) public ERC20("FIO Protocol", "wFIO") {
         _mint(msg.sender, _initialSupply);
