f425c3043c943e550f7063bdb74b131e96bf755c
Adam Androulidakis <37982984+adsorptionenthalpy@users.noreply.github.com>
2021-02-10 15:47:25 -0700

    remove unused address

--- contracts/wfio.sol
+++ contracts/wfio.sol
@@ -4,12 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WFIO is ERC20 {
 
-    address private owner;
-
     constructor(uint256 _initialSupply) public ERC20("FIO Protocol", "wFIO") {
         _mint(msg.sender, _initialSupply);
         _setupDecimals(9);
-        owner = msg.sender;
     }
 
 }
