b03e8f0a05689848ff97c6cab4f7b3c371e86c81
BlockTomeLTD <BlockTomeLTD@gmail.com>
2021-02-10 18:06:27 -0700

    make pausable

--- contracts/wfio.sol
+++ contracts/wfio.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract WFIO is ERC20, ERC20Burnable {
+contract WFIO is ERC20Burnable, ERC20Pausable {
 
     constructor(uint256 _initialSupply) public ERC20("FIO Protocol", "wFIO") {
         _mint(msg.sender, _initialSupply);
         _setupDecimals(9);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 
 }
